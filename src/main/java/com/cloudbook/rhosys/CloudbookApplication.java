dad2fcccf759fc07b628018d7dca02845e65959f
Naveen <naveen.nare@gmail.com>
2017-10-02 21:37:19 -0400

    Initial Setup
    Change-Id: Ib5f725368dc2398f6dd4f7015ba19d51a4403031

---
+++ src/main/java/com/cloudbook/rhosys/CloudbookApplication.java
@@ -0,0 +1,12 @@
+package com.cloudbook.rhosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbookApplication.class, args);
+	}
+}

656cfdc659e94943ddaf28da354a7d671ae00fd0
Naveen <naveen.nare@gmail.com>
2017-10-02 22:16:28 -0400

    Home Controller Spring Security Setup

--- src/main/java/com/cloudbook/rhosys/CloudbookApplication.java
+++ src/main/java/com/cloudbook/rhosys/CloudbookApplication.java
@@ -1,12 +1,29 @@
 package com.cloudbook.rhosys;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudbookApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String bean: beanNames) {
+				System.out.println(bean);
+			}
+			
+		};
 	}
 }

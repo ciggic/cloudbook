bd13c4b3e3b0c334c0da7767858d28d5e0823f99
Naveen <naveen.nare@gmail.com>
2017-10-12 23:29:46 -0400

    Security Issues have been fixed
    Please use the below credentials
    
    username : user
    password: password

--- src/main/java/com/cloudbook/rhosys/CloudbookApplication.java
+++ src/main/java/com/cloudbook/rhosys/CloudbookApplication.java
@@ -7,15 +7,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@ComponentScan("com.cloudbook")
+//@ComponentScan("com.cloudbook")
 @SpringBootApplication
-public class CloudbookApplication {
+public class CloudbookApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudbookApplication.class, args);
 	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+	}
+	
 	
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
--- src/main/java/com/cloudbook/rhosys/security/SecurityConfig.java
+++ src/main/java/com/cloudbook/rhosys/security/SecurityConfig.java
@@ -1,16 +1,29 @@
 package com.cloudbook.rhosys.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	protected void configure(HttpSecurity security) throws Exception {
+		//@// @formatter:off
 		security.authorizeRequests()
 			.anyRequest().authenticated()
+			.antMatchers("/css/**").permitAll()
 			.and()
-			.formLogin().loginPage("/login");
+			.formLogin()
+			.loginPage("/login").permitAll()
+			.failureUrl("/login?error").permitAll()
+			.and()
+			.logout().permitAll();
+		// @formatter:on
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
 	}
 }

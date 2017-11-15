439f5870039ad79cceaf0b895195c41b58913432
Naveen <naveen.nare@gmail.com>
2017-11-14 22:16:05 -0500

    Header Bar Added

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ const css= require('./css/app.css');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App'; // there is difference between {App} vs import App from "./App"
+import HeaderBar from './HeaderBar'; // there is difference between {App} vs import App from "./App"
 
 
 ReactDOM.render(
-  <App />,
+  <HeaderBar />,
   document.getElementById('root')
 );

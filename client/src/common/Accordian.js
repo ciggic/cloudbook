4aeaa5b9fe06090a276b5338940a53b47a16733b
Basanth P Rangadhol <basanth.rangadhol@tavant.com>
2017-11-15 07:45:47 +0530

    Trying to restore back the files, that got deleted accidentally

---
+++ client/src/common/Accordian.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+
+
+class extends React.Component {
+    render () {
+      return () {
+        <div>
+            
+        </div>
+      }
+    }
+}
